c68b29d8f400e319df1ae4f0146d41c41e406e11
Ambroise-ADANLEDJI <adanledjiambroise@gmail.com>
2023-08-21 13:32:45 +0200

    Mise en place du système routage dans la page index.

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php 
+
+abstract class Database
+{
+    private static $pdo;
+
+    private static function setConnection()
+    {
+        self::$pdo = new PDO("mysql:host=localhost;dbname=gestion_sport", "root", "");
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    protected function getConnection()
+    {
+        if(self::$pdo == null)
+        {
+            self::setConnection();
+        }
+        
+        return self::$pdo;
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,4 +1,52 @@
 <?php 
 
-echo "Hello World!";
 
+// Mise en place du système de routage
+
+            // Définition constance une route absolue vers les ressources du site
+            define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") . 
+            "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
+            //  http://localhost/ressourcedemandée.
+            
+            
+            try{
+            
+                if(empty($_GET['page'])){
+            
+                    throw new Exception("La page n'existe pas");
+            
+                }else{
+                    $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
+            
+                    switch($url[0]){
+                        case 'admin': 
+                            if(empty($url[1])) throw new Exception("La page n'existe pas");
+                            switch ($url[1]){
+                                case 'login' : echo "La page login"; 
+                                    break;
+                                case 'connexion' : echo "La page connexion";
+                                    break;
+                                case 'adminPanel' : echo "La page adminPanel";
+                                    break;
+                                case 'deconnexion' : echo "La page deconnexion";
+                                    break;
+                                default: throw new Exception("La page n'existe pas");
+
+                            }
+                            break;
+                        case 'front' : 
+                            if(empty($url[1])) throw new Exception("La page n'existe pas");
+                            switch ($url[1]){
+                                case 'home' : echo "La page home";
+                                    break;
+                                case 'contact' : echo "La page contact";
+                                    break;
+                                default: throw new Exception("La page n'existe pas");
+                            }
+                            break;
+                    }
+                }
+            
+            }catch(PDOException $e){
+                echo $e->getMessage();
+            }
